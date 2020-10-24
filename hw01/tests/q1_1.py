2f81333d6380b20dbae1d4a3b28b0c0f0c5cc08d
ykk <ykk>
2020-10-23 19:48:30 -0700

    Added HW01

---
+++ hw01/tests/q1_1.py
@@ -0,0 +1,1 @@
+test = {'name': 'q1_1', 'points': 1, 'suites': [{'cases': [{'code': '>>> 1 <= hello_q1 <= 5\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ hw01/tests/q1_5.py
@@ -0,0 +1,1 @@
+test = {'name': 'q1_5', 'points': 1, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
